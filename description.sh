b97a24235aad7ce40ef08e1a1b5140835368fe86
Amina <157822543+Amina0011@users.noreply.github.com>
2024-08-13 01:43:57 +0500

    Add files via upload

---
+++ description.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Function to pause and display messages with a delay
+pause() {
+    sleep 1
+}
+
+# Print the challenge description with pauses
+echo "Why don't you try your luck?"
+pause
+echo "Maybe you will find the password for the next challenge there!"
+pause
+echo
+echo "But be careful of rotations :)"
